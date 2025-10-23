e638e46d66a1b3e69b61a9e710dfac95ff0c98a2
Ben Cuttance <bcuttance1998@gmail.com>
2025-10-24 00:24:22 +1030

    FEAT(footer): Added filler links, added svg social media icons

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	import { Facebook } from '@lucide/svelte';
+	import { Instagram } from '@lucide/svelte';
+	import { Twitter } from '@lucide/svelte';
 
-let links= {
-    home: "/",
-    catalog: "/",
-    about: "/",
-    contactUs: "/",
-}
+	let facebookHovered = false;
+	let instagramHovered = false;
+    let twitterHovered = false;
+
+	let links = {
+		home: '/',
+		catalog: '/',
+		about: '/',
+		contactUs: '/'
+	};
 </script>
 
 <section>
@@ -19,7 +26,37 @@ let links= {
 			<a href={links.contactUs}>Contact Us</a>
 		</div>
 	</div>
-	<div>Follow us here</div>
+	<div>
+		<button
+			onmouseenter={() => (facebookHovered = true)}
+			onmouseleave={() => (facebookHovered = false)}
+			onfocus={() => (facebookHovered = true)}
+			onblur={() => (facebookHovered = false)}
+			aria-label="Facebook link"
+		>
+			<Facebook color={facebookHovered ? 'red' : 'white'} size={50} />
+		</button>
+
+		<button
+			onmouseenter={() => (instagramHovered = true)}
+			onmouseleave={() => (instagramHovered = false)}
+			onfocus={() => (instagramHovered = true)}
+			onblur={() => (instagramHovered = false)}
+			aria-label="Instagram link"
+		>
+			<Instagram color={instagramHovered ? 'red' : 'white'} size={50} />
+		</button>
+
+		<button
+			onmouseenter={() => (twitterHovered = true)}
+			onmouseleave={() => (twitterHovered = false)}
+			onfocus={() => (twitterHovered = true)}
+			onblur={() => (twitterHovered = false)}
+			aria-label="Twitter link"
+		>
+			<Twitter color={twitterHovered ? 'red' : 'white'} size={50} />
+		</button>
+	</div>
 	<div>Brand Logo?</div>
 </section>
 
@@ -29,41 +66,50 @@ let links= {
 		font-family: var(--font-main);
 	}
 
-section {
-  height: 300px;
-  background-color: var(--main-light-black);
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  text-align: center; 
-  padding: 1rem;
-  align-items: center; 
-  justify-items: center; 
-  color: white;
-  font-family: var(--font-main);
-}
+	section {
+		height: 300px;
+		background-color: var(--main-light-black);
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		text-align: center;
+		padding: 1rem;
+		align-items: center;
+		justify-items: center;
+		color: white;
+		font-family: var(--font-main);
+	}
 
-#link-container {
-  display: flex;
-  flex-direction: column;
-  text-align: left; 
-  align-items: flex-start; 
-}
+	#facebook-icon {
+		width: 100px;
+		height: 100px;
+	}
 
-#links {
-  display: flex;
-  flex-direction: column;
-  gap: 0.3rem;
-}
+	#link-container {
+		display: flex;
+		flex-direction: column;
+		text-align: left;
+		align-items: flex-start;
+	}
 
-a {
-  text-decoration: none;
-  transition: 0.2s;
-  font-size: 20px;
-}
+	#links {
+		display: flex;
+		flex-direction: column;
+		gap: 0.3rem;
+	}
 
-a:hover {
-  color: red;
-  transform: translateX(5px);
-}
+	a {
+		text-decoration: none;
+		transition: 0.2s;
+		font-size: 20px;
+	}
 
+	a:hover {
+		color: red;
+		transform: translateX(5px);
+	}
+
+	button {
+		background-color: transparent;
+		border: none;
+	}
 </style>
