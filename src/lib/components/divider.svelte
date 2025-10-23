df934095ff4ad4b612ab24be0c29476061e5e27b
Ben Cuttance <bcuttance1998@gmail.com>
2025-10-23 23:07:20 +1030

    FEAT(css changes): Added css root variable to keep colors consistant across app

--- src/lib/components/divider.svelte
+++ src/lib/components/divider.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-	let { color }: { color: string } = $props();
+	let { color, text  }: { color: string, text: string } = $props();
 </script>
 
-<section id="divider" style="background-color: {color}"></section>
+<section id="divider" style="background-color: {color}"><h2>{text}</h2></section>
 
 <style>
 	section {
 		height: 100px;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	h2{
+		font-family: var(--font-main);
+		color: white;
 
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,29 @@
 	import Header from "../lib/components/header.svelte";
 	import HeroSection from "$lib/components/heroSection.svelte";
 	import Divider from "$lib/components/divider.svelte";
+	import Footer from "$lib/components/footer.svelte";
 
 </script>
 
+<section>
 <Header/>
 <HeroSection/>
-<Divider color=black/>
+<Divider color="var(--main-light-black)" text=""/>
+
+<div>Main Here</div>
+<Divider color="var(--main-light-black)" text="Something to go here"/>
+<Footer/>
+
+</section>
+
+<style>
+	section{
+		display: grid;
+		min-height: 100vh;
+		grid-template-rows: auto auto 1fr auto;
+	}
+</style>
+
 
 
 
