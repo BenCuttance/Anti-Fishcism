ae96f39557e00d631b7eb16934d8954432e3977f
Ben Cuttance <bcuttance1998@gmail.com>
2025-11-08 22:47:17 +1030

    FEAT(contact-us): Added contact us page files. Added basic form skeleton

--- README.md
+++ README.md
@@ -2,10 +2,13 @@ TODO -
 
 - Click off of hamburger menu - (DONE)
 
+- Add contact us page
+  - Contact form 
+
 - Add about me page
   - get image for banner (DONE)
   - Add description (DONE)
-  - Fix description, betters colours???
+  - Fix description, betters colours (DONE)
 
 - CSS for smaller screens
   - Home (DONE)
---
+++ src/routes/contactUs/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<section>
+	<form action="" method="post">
+		<label for="fname">First name:</label><br />
+		<input type="text" id="fname" name="fname" /><br />
+		<label for="lname">Last name:</label><br>
+		<input type="text" id="lname" name="lname" /> <br>
+		<label for="message">Message:</label><br>
+		<input type="text" name="message" id="message" /><br>
+		<button>Button</button>
+	</form>
+</section>
+
+<style>
+</style>
