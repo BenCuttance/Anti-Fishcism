8141678a98dd8eb72944052ff0403c83cea3732b
Ben Cuttance <bcuttance1998@gmail.com>
2025-11-18 21:41:05 +1030

    FEAT(stripe call): Added try catch block

--- src/routes/api/checkout/+server.ts
+++ src/routes/api/checkout/+server.ts
@@ -1,27 +1,34 @@
 import { SECRET_STRIPE_KEY } from '$env/static/private';
 import { PUBLIC_FRONTEND_URL } from '$env/static/public';
-import { json } from '@sveltejs/kit';
+import { json } from 'stream/consumers';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(SECRET_STRIPE_KEY);
 
 export async function POST({ request }) {
-    const { priceId } = await request.json();
+	try {
+		const { priceId } = await request.json();
+		const session = await stripe.checkout.sessions.create({
+			mode: 'payment',
+			line_items: [
+				{
+					price: priceId,
+					quantity: 1
+				}
+			],
+			//TODO: Keep return to success and failure? Or return to catalogue?
+			success_url: `${PUBLIC_FRONTEND_URL}/success`,
+			cancel_url: `${PUBLIC_FRONTEND_URL}/failure`
+		});
 
-    const session = await stripe.checkout.sessions.create({
-        mode: "payment",
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1,
-            },
-        ],
-        //TODO: Keep return to success and failure? Or return to catalogue?
-        success_url: `${PUBLIC_FRONTEND_URL}/success`,
-        cancel_url: `${PUBLIC_FRONTEND_URL}/failure`,
-    });
-
-    return new Response(JSON.stringify({ url: session.url }), {
-        headers: { "Content-Type": "application/json" }
-    });
+		return new Response(JSON.stringify({ url: session.url }), {
+			headers: { 'Content-Type': 'application/json' }
+		});
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			return new Response(JSON.stringify({ error: err.message }), { status: 500 });
+		} else {
+            return new Response(JSON.stringify({ error: "unknown error" }))
+        }
+	}
 }
